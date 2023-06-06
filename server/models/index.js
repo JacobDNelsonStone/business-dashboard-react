d36d445089f0ac3f50e7efa1110de6f53b986dd9
Mohamed <amiiini44@hotmail.com>
2023-06-06 12:50:06 -0500

    set ups

---
+++ server/models/index.js
@@ -0,0 +1,4 @@
+const Employee = require("./Employee");
+const DepartmentStats = require("./DepartmentStats");
+const Meeting = require("./Meeting");
+module.exports = { Employee, DepartmentStats, Meeting };
