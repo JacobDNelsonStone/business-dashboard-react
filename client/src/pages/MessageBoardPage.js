0252a6cbdad467b96c8d7f581e46e4e2786d9de6
Jacob Nelson-Stone <nelsonjacobmusic@gmail.com>
2023-06-07 17:41:00 -0500

    got content from db to display on message board, working on styling

--- client/src/pages/MessageBoardPage.js
+++ client/src/pages/MessageBoardPage.js
@@ -6,17 +6,21 @@ import { Col, Container } from "react-bootstrap"
 function MessageBoard({ children }) {
 
   return (
-    <Container className="col-xl-12 d-flex flex-row">
-      <Col className="col-xl-3 col-9">
-        <EmployeeList>
-          {children}
-        </EmployeeList>
-      </Col>
-      <Col className="col-xl-9 d-flex">
-        <MessageList>
-          { children }
-        </MessageList>
-      </Col>
+    <Container className="col-xl-12 d-flex flex-row ms-0">
+      <Container className="col-xl-3 align-items-start">
+        <Col className="col-xl-12 col-9 px-5">
+          <EmployeeList>
+            {children}
+          </EmployeeList>
+        </Col>
+      </Container>
+      <Container className="ms-5 col-xl-9 d-flex align-items-end ">
+        <Col className="">
+          <MessageList>
+            {children}
+          </MessageList>
+        </Col>
+      </Container>
     </Container>
   )
 }
