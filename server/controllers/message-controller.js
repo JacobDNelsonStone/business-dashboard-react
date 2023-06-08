ea94e1c906fecd21df84221e0da8f9a2d3895a9d
Jacob Nelson-Stone <nelsonjacobmusic@gmail.com>
2023-06-08 10:47:49 -0500

    figured out employee population in message controller

--- server/controllers/message-controller.js
+++ server/controllers/message-controller.js
@@ -19,7 +19,8 @@ module.exports = {
 
   async getMessages(req, res) {
     try {
-      const allMessages = await Message.find().populate('Employee')
+      
+      const allMessages = await Message.find().populate('employeeId')
       // const allMessages = await messages.forEach(message => message.populate('Employee'))
 
       res.status(200).json({status: 'success', payload: allMessages})
