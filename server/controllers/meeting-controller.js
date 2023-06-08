89216e4087e83d52be2b5346909199d13e2f84b2
Jacob Nelson-Stone <nelsonjacobmusic@gmail.com>
2023-06-08 17:39:41 -0500

    finished get route for all meetings

--- server/controllers/meeting-controller.js
+++ server/controllers/meeting-controller.js
@@ -3,12 +3,17 @@ const { Meeting } = require('../models');
 // FYI: The user's password is encrypted at the model level
 
 module.exports = {
-  
+
   async createMeeting({ body, params }, res) {
     try {
-      const meeting = await Meeting.create({ meetingTopic: body.meetingTopic, meetingDate: body.meetingDate, employees: body.employees })
-      return res.status(200).json({ status: "success", payload: meeting})
-    } catch(err){
+      const meeting = await Meeting.create(
+        {
+          meetingTopic: body.meetingTopic,
+          meetingDate: body.meetingDate,
+          employees: body.employees
+        })
+      return res.status(200).json({ status: "success", payload: meeting })
+    } catch (err) {
       return res.status(400).json({ status: "error", msg: `Error creating meeting Item: ${err.message}` })
     }
   },
@@ -16,10 +21,11 @@ module.exports = {
 
   async getAllMeetings({ body, params }, res) {
     try {
-      const meetings = await Meeting.find().populate('employees')
+      const meetings = await Meeting.find()
+        .populate('employees')
       console.log(meetings)
       return res.status(200).json({ status: "success", payload: meetings })
-    } catch(err){
+    } catch (err) {
       console.log(err.message)
       return res.status(400).json({ status: "error", msg: `Error retrieving meetings: ${err.message}` })
     }
@@ -30,7 +36,7 @@ module.exports = {
     try {
       const meeting = await Meeting.findOne({ _id: params.id })
       return res.status(200).json({ status: "success", payload: meeting })
-    } catch(err){
+    } catch (err) {
       return res.status(400).json({ status: "error", msg: `Error retrieving meeting Item: ${err.message}` })
     }
   },
@@ -39,7 +45,7 @@ module.exports = {
     try {
       const meeting = await Meeting.findOneAndUpdate({ _id: params.id }, { body }, { new: true })
       return res.status(200).json({ status: "success", payload: meeting })
-    } catch(err){
+    } catch (err) {
       console.log(err.message)
       return res.status(400).json({ status: "error", msg: `Error updating meeting Item: ${err.message}` })
     }
@@ -50,7 +56,7 @@ module.exports = {
     try {
       const meeting = await Meeting.findOneAndRemove({ _id: params.id })
       return res.status(200).json({ status: "success", payload: meeting })
-    } catch(err){
+    } catch (err) {
       return res.status(400).json({ status: "error", msg: `Error removing meeting Item: ${err.message}` })
     }
   },
